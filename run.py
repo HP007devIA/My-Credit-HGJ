58c7cb5253b1f9c6a808670b9ca521a97b76d6b3
Harold-102-103 <h.petiard@102-103.com>
2023-10-04 16:48:37 +0200

    push_test

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+
+
+class Square(BaseModel):
+    age : int
 
 app = FastAPI(
     title= "Myfirst API",
@@ -13,10 +18,15 @@ This is your description app, written in markdown code
 
 n=None
 
-@app.get('/square')
-def square(n: int= None) -> int:
-    if n == None: return "Please enter a number"
-    else:return int(n)*int(n)
+# @app.get('/square')
+# def square(n: int= None) -> int:
+#     if n == None: return "Please enter a number"
+#     else:return int(n)*int(n)
+
+@app.post('/square')
+def square(s:Square) -> str:
+    if s.n == None: return "Please enter a number"
+    else:return str(int(s.n)*int(s.n))
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
