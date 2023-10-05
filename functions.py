c0cd4450174ace308331a3eefb87371ec21b8b61
nelguibson <g.zouiten@laposte.net>
2023-10-05 12:30:22 +0200

    reconnaisse

--- functions.py
+++
@@ -1,3 +0,0 @@
-def prediction_credit(age, job, marital, education, default, balance, housing, loan, contact, day, month, duration, campaign, pdays, previous, poutcome):
-    prediction = model.predict([[age, job, marital, education, default, balance, housing, loan, contact, day, month, duration, campaign, pdays, previous, poutcome]])
-    return prediction
