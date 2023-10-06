8bf90573c4b632293b0e43a7eb8eefdc08294049
HP007devIA <harold.petiard@isen-ouest.yncrea.fr>
2023-10-06 12:40:08 +0200

    ajout requirements / api ok

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from fastapi import FastAPI
 import uvicorn
-import functions
+
+from pydantic import BaseModel
+import pandas as pd
+import pickle
+
+# Charger le modèle depuis un fichier Pickle
+with open('modele_random_forest.pickle', 'rb') as f:
+    modele = pickle.load(f)
+
+# Créez un DataFrame à partir de votre dictionnaire
+data_dict = {'age': 30, 'job': 'services', 'marital': 'married', 'education': 'tertiary', 'default': 'no', 'balance': 1350, 'housing': 'yes', 'loan': 'no', 'contact': 'cellular', 'day': 16, 'month': 'oct', 'duration': 185, 'campaign': 1, 'pdays': 330, 'previous': 1, 'poutcome': 'other'}
+
+
+class Config_donnees(BaseModel):
+    age:int
+    job:str
+    marital:str
+    education:str
+    default:str
+    balance:int
+    housing:str
+    loan:str
+    contact:str
+    day:int
+    month:str
+    duration:int
+    campaign:int
+    pdays:int
+    previous:int
+    poutcome:str
 
 
 
@@ -12,8 +41,16 @@ Descritption de l'api ici !!!
 )
 
 # Définir une route POST pour la commande
-@app.post("/bank_loan", response_model=functions.reponse_model, summary="Prédictions")
-def predict(n:functions.Config_donnees):
+@app.post("/bank_loan")
+def predict(data_df: Config_donnees):
+    data_df = pd.DataFrame([data_dict])
+
+        # Utilisez le modèle pour effectuer des prédictions
+    predictions = modele.predict(data_df)
+
+    # Extrait la valeur de la prédiction (la première prédiction s'il y en a plusieurs)
+    prediction = predictions[0]
+    return prediction
     """
     Obtenez un avis de prêt bancaire en utilisant les paramètres suivants:
     - **age**:30,
--- functions.py
+++ functions.py
@@ -3,10 +3,10 @@ import numpy as np
 from pydantic import BaseModel
 
 
-# # importer le modèle, les scalerer et les labelencoderoder
-encoder = pickle.load("encoders")
-model = pickle.load("model")
-scaler = pickle.load("scalers")
+# # # importer le modèle, les scalerer et les labelencoderoder
+# encoder = pickle.load("encoders")
+# model = pickle.load("model")
+# scaler = pickle.load("scalers")
 
 class Config_donnees(BaseModel):
     age:int
@@ -32,52 +32,52 @@ class reponse_model(BaseModel):
     importance:list
 
 
-def scaler_lab(n:dict) ->list:
-    """
-    Fonction servant à standardiser et labéliser les donnees
-    Sortie de type : [0.12,0.55,0.56....]
-    """
-    transformed_data=[]
+# def scaler_lab(n:dict) ->list:
+#     """
+#     Fonction servant à standardiser et labéliser les donnees
+#     Sortie de type : [0.12,0.55,0.56....]
+#     """
+#     transformed_data=[]
     
-    transformed_data.append(scaler['age'].transform(np.array([n.age]).reshape(-1,1))[0][0])
+#     transformed_data.append(scaler['age'].transform(np.array([n.age]).reshape(-1,1))[0][0])
     
-    job = encoder['job'].transform([n.job])[0]
-    transformed_data.append(scaler['job'].transform(np.array([job]).reshape(-1,1))[0][0])
+#     job = encoder['job'].transform([n.job])[0]
+#     transformed_data.append(scaler['job'].transform(np.array([job]).reshape(-1,1))[0][0])
     
-    marital = encoder['marital'].transform([n.marital])[0]
-    transformed_data.append(scaler['marital'].transform(np.array([marital]).reshape(-1,1))[0][0])
+#     marital = encoder['marital'].transform([n.marital])[0]
+#     transformed_data.append(scaler['marital'].transform(np.array([marital]).reshape(-1,1))[0][0])
     
-    education = encoder['education'].transform([n.education])[0]
-    transformed_data.append(scaler['education'].transform(np.array([education]).reshape(-1,1))[0][0])
+#     education = encoder['education'].transform([n.education])[0]
+#     transformed_data.append(scaler['education'].transform(np.array([education]).reshape(-1,1))[0][0])
     
-    default = encoder['default'].transform([n.default])[0]
-    transformed_data.append(scaler['default'].transform(np.array([default]).reshape(-1,1))[0][0])
-    transformed_data.append(scaler['balance'].transform(np.array([n.balance]).reshape(-1,1))[0][0])
+#     default = encoder['default'].transform([n.default])[0]
+#     transformed_data.append(scaler['default'].transform(np.array([default]).reshape(-1,1))[0][0])
+#     transformed_data.append(scaler['balance'].transform(np.array([n.balance]).reshape(-1,1))[0][0])
     
-    housing= encoder['housing'].transform([n.housing])[0]
-    transformed_data.append(scaler['housing'].transform(np.array([housing]).reshape(-1,1))[0][0])
+#     housing= encoder['housing'].transform([n.housing])[0]
+#     transformed_data.append(scaler['housing'].transform(np.array([housing]).reshape(-1,1))[0][0])
     
-    loan = encoder['loan'].transform([n.loan])[0]
-    transformed_data.append(scaler['loan'].transform(np.array([loan]).reshape(-1,1))[0][0])
+#     loan = encoder['loan'].transform([n.loan])[0]
+#     transformed_data.append(scaler['loan'].transform(np.array([loan]).reshape(-1,1))[0][0])
     
-    contact = encoder['contact'].transform([n.contact])[0]
-    transformed_data.append(scaler['contact'].transform(np.array([contact]).reshape(-1,1))[0][0])
+#     contact = encoder['contact'].transform([n.contact])[0]
+#     transformed_data.append(scaler['contact'].transform(np.array([contact]).reshape(-1,1))[0][0])
     
-    transformed_data.append(scaler['balance'].transform(np.array([n.day]).reshape(-1, 1))[0][0])
-    month = encoder['month'].transform([n.month])[0]
-    transformed_data.append(scaler['month'].transform(np.array([month]).reshape(-1,1))[0][0])
+#     transformed_data.append(scaler['balance'].transform(np.array([n.day]).reshape(-1, 1))[0][0])
+#     month = encoder['month'].transform([n.month])[0]
+#     transformed_data.append(scaler['month'].transform(np.array([month]).reshape(-1,1))[0][0])
     
-    transformed_data.append(scaler['duration'].transform(np.array([n.duration]).reshape(-1, 1))[0][0])
+#     transformed_data.append(scaler['duration'].transform(np.array([n.duration]).reshape(-1, 1))[0][0])
     
-    transformed_data.append(scaler['campaign'].transform(np.array([n.campaign]).reshape(-1, 1))[0][0])
+#     transformed_data.append(scaler['campaign'].transform(np.array([n.campaign]).reshape(-1, 1))[0][0])
     
-    transformed_data.append(scaler['pdays'].transform(np.array([n.pdays]).reshape(-1, 1))[0][0])
+#     transformed_data.append(scaler['pdays'].transform(np.array([n.pdays]).reshape(-1, 1))[0][0])
     
-    transformed_data.append(scaler['previous'].transform(np.array([n.previous]).reshape(-1, 1))[0][0])
+#     transformed_data.append(scaler['previous'].transform(np.array([n.previous]).reshape(-1, 1))[0][0])
     
-    poutcome=encoder['poutcome'].transform([n.poutcome])[0]
-    transformed_data.append(scaler['poutcome'].transform(np.array([poutcome]).reshape(-1,1))[0][0])
-    return transformed_data
+#     poutcome=encoder['poutcome'].transform([n.poutcome])[0]
+#     transformed_data.append(scaler['poutcome'].transform(np.array([poutcome]).reshape(-1,1))[0][0])
+#     return transformed_data
 
 def predictions(data:list) -> dict:
     """
